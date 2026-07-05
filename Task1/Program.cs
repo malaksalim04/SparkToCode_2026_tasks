7dc3c833c347b79050a422cda973b893ac33ed32
malaksalim <malakalburtmani@gmail.com>
2026-07-05 19:41:13 +0400

    update all the files

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // task1 personal info card 
+        string name = "Sara";
+        int age = 21;
+        double height = 1.65;
+        bool student = true;
+        
+        Console.Write("Name :" + name+", Age :"+age+", Height :"+ height+", Student :" +student  );
+
+    }
+}
