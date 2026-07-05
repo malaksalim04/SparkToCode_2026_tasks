45539b4dd1514f7d3f1fd7e8dbc02fd1b3feb673
malaksalim <malakalburtmani@gmail.com>
2026-07-06 01:33:34 +0400

    task1 countdown timer

---
+++ tssk2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace tssk2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       // task1 countdown timer 
+        
+        Console.WriteLine("enter the starting number: ");
+        int start = int.Parse(Console.ReadLine());
+
+
+        for (int i = start; i >= 1; i--)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("liftoff ");
+
+    }
+}
