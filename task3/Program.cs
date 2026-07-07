79e784cf10004e56b6be348c25bb720616f005c4
malaksalim <malakalburtmani@gmail.com>
2026-07-07 11:07:07 +0400

    task2 power & root explorer

--- task3/Program.cs
+++ task3/Program.cs
@@ -15,5 +15,18 @@ class Program
         
         Console.WriteLine("positive difference: " +difference);
         
+        //task2 power & root explorer
+        Console.Write("enter a number: ");
+        double num3 = Convert.ToDouble(Console.ReadLine());
+
+        double square = Math.Pow(num3, 2);
+        double squareroot = Math.Sqrt(num3);
+        Console.WriteLine("square: " + square);
+        Console.WriteLine("square root : " + squareroot);
+        
+        
+        
+
+
     }
 }
