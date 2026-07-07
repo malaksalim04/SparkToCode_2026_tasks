186e8dc8cac943af033cf26fece2d9b6777745d0
malaksalim <malakalburtmani@gmail.com>
2026-07-07 10:56:43 +0400

    task1 absolute difference

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace task3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //task1 absolute difference 
+        Console.Write("enter the frist number: ");
+        double num1 = double.Parse(Console.ReadLine());
+        
+        Console.Write("enter the seconsd number: ");
+        double num2 = double.Parse(Console.ReadLine());
+
+        double difference = Math.Abs(num1 - num2);
+        
+        Console.WriteLine("positive difference: " +difference);
+        
+    }
+}
