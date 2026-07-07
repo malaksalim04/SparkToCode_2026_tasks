2c2e529c4a410120e411a48bf3b33fdeed33431f
malaksalim <malakalburtmani@gmail.com>
2026-07-07 14:31:15 +0400

    task1 fixed grades array

---
+++ task4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace task4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //task1 fixed grades array 
+        int[] grades = new int[5];
+        //input grades using a for loop 
+        for (int i = 0; i < grades.Length; i++)
+        {
+            Console.Write("enter grade " + (i + 1) + ": ");
+            grades[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        //print grades using a foreach loop
+        Console.WriteLine("\nStudent Grades: ");
+        foreach (int grade in grades)
+        {
+            Console.WriteLine(grade);
+        }
+    }
+}
