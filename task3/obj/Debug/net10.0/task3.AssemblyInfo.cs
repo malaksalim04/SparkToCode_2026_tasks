cd0e49c9ddbe5bb77c1d2d1c3149984abb1ea6a3
malaksalim <malakalburtmani@gmail.com>
2026-07-08 11:10:44 +0400

    add project files

--- task3/obj/Debug/net10.0/task3.AssemblyInfo.cs
+++ task3/obj/Debug/net10.0/task3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9b29d29f3e9f2d778fa88e7c8a5d9354bb91322")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42431896c0484ad290cf5b7a8de72c83ab9d6f7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("task3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- task4/obj/Debug/net10.0/task4.AssemblyInfo.cs
+++ task4/obj/Debug/net10.0/task4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+783cd378488b58d1bd9abf699a017fd452c6443f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42431896c0484ad290cf5b7a8de72c83ab9d6f7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("task4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
