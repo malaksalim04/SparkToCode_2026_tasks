bfc3783b0923d6316d9d72ea76b43a513f1b5480
malaksalim <malakalburtmani@gmail.com>
2026-07-08 11:21:59 +0400

    task1 personalized welcome function

---
+++ task5/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace task5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //task1 personalized welcome function 
+        //function with one parameter and no return value 
+
+        static void PrintWelcome(string name)
+        {
+            Console.WriteLine("welcome, " + name +"!");
+            Console.WriteLine("we hope you have a great day ");
+        }
+
+        static void main(string[] args)
+        {
+            Console.Write("enter your name: ");
+            string name = Console.ReadLine();
+            //call the function 
+            PrintWelcome(name);
+        }
+    }
+}
