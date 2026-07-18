dea1f4ade51fd5064d15dbf0c5dcb14265d4bab1
malaksalim <malakalburtmani@gmail.com>
2026-07-18 18:54:41 +0400

    add sturcture for the code

---
+++ OOP_Part2 /Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OOP_Part2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Room> rooms = new List<Room>();
+        List<Guest> guests = new List<Guest>();
+        //menu 
+    }
+}
+
+class Room
+{
+    //room
+}
+
+class Guest
+{
+    //guest
+}
